b9ceb98fe61cc99e275c019c4136b0254486dc62
davidaruldass <davidaruldass@yahoo.com>
2019-11-22 14:43:41 -0700

    css

--- src/app/contacts/contacts-filter.pipe.ts
+++ src/app/contacts/contacts-filter.pipe.ts
@@ -7,18 +7,30 @@ import { Contact } from './contact.model';
 })
 export class ContactsFilterPipe implements PipeTransform {
 
-  transform(contacts: Contact[], term: string[]): any {
-    let filteredContacts: Contact[] = [];
+  // transform(contacts: Contact[], term: string[]): any {
+  //   let filteredArray: Contact[] = [];
 
-   
-   
+
+  transform(contacts: Contact[], [term]) {
+    let filteredArray: Contact[] = [];
+
+    for (let i = 0; i < contacts.length; i++) {
+      let contact = contacts[i];
+      if (contact.name.toLowerCase().includes(term)) {
+        filteredArray.push(contact);
+      }
+    }
+
+    if (filteredArray.length < 1) {
+      return contacts;
+    }
+    return filteredArray;
+
     // if (term && term.length > 0) {
     //   contact = contacts.filter(
     //     contact.name.toLowerCase().includes(term.toLowerCase())
     //   );
     // }
     // return filteredContacts.length > 0 ? filteredContacts : contacts;
-
-
-   }
+  }
 }
